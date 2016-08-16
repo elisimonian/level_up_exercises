3d8092cc03436580d423260cf90d938c832bb5c7
esimonian <esimonian16@gmail.com>
2016-08-16 12:18:26 +0700

    triangle/arrowhead

--- arrowhead/arrowhead.rb
+++ arrowhead/arrowhead.rb
@@ -1,34 +1,49 @@
 class Arrowhead
+  attr_accessor :region, :shape
   # This seriously belongs in a database.
   CLASSIFICATIONS = {
     far_west: {
       notched: "Archaic Side Notch",
       stemmed: "Archaic Stemmed",
       lanceolate: "Agate Basin",
-      bifurcated: "Cody",
+      bifurcated: "Cody"
     },
     northern_plains: {
       notched: "Besant",
       stemmed: "Archaic Stemmed",
       lanceolate: "Humboldt Constricted Base",
-      bifurcated: "Oxbow",
-    },
+      bifurcated: "Oxbow"
+    }
   }
 
   # FIXME: I don't have time to deal with this.
-  def self.classify(region, shape)
-    if CLASSIFICATIONS.include? region
-      shapes = CLASSIFICATIONS[region]
-      if shapes.include? shape
-        arrowhead = shapes[shape]
-        "You have a(n) '#{arrowhead}' arrowhead. Probably priceless."
-      else
-        raise "Unknown shape value. Are you sure you know what you're talking about?"
-      end
+  def initialize(region, shape)
+    @region = region
+    @shape = shape
+  end
+  
+  def classify
+    check_region
+    check_shape
+  end
+
+  def check_region
+    if CLASSIFICATIONS.include? @region
+      @shapes = CLASSIFICATIONS[@region]
     else
       raise "Unknown region, please provide a valid region."
     end
   end
+
+  def check_shape
+    if @shapes.include? @shape
+      arrowhead = @shapes[@shape]
+      "You have a(n) '#{arrowhead}' arrowhead. Probably priceless."
+    else
+      raise "Unknown shape value. Are you sure you know what you're talking about?"
+    end
+  end
 end
 
-puts Arrowhead.classify(:northern_plains, :bifurcated)
+arrowhead = Arrowhead.new(:northern_plains, :bifurcated)
+puts arrowhead .classify
